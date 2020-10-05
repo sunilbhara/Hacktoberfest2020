c46439eda6c1cdb3c63829a1be5a7df30582bb33
Vishvam04 <34434716+Vishvam04@users.noreply.github.com>
2020-10-05 23:02:06 +0530

    Create CountingSort.java

---
+++ Java/CountingSort.java
@@ -0,0 +1,32 @@
+public class CountingSort {
+
+    static int[] countingSort(int[] nums, int maxNumber) {
+        int[] temp = new int[maxNumber + 1];
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            temp[nums[i]]++;
+        }
+        
+        int index = 0;
+        for(int i=0; i<temp.length; i++){
+            while(temp[i] > 0){
+                result[index++] = i;
+                temp[i]--;
+            }
+        }
+        
+        return result;
+    }
+
+    static void printArray(int[] nums) {
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 5, 6, 7, 1, 9, 0, 8, 6};
+        nums = countingSort(nums, 10);
+        printArray(nums);
+    }
+}
