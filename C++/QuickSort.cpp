08b0463a4b170cfd0581a96c51fce5a2a9c20455
Aswin1362 <70067393+Aswin1362@users.noreply.github.com>
2020-10-07 20:49:14 +0530

    Update QuickSort.cpp

--- C++/QuickSort.cpp
+++ C++/QuickSort.cpp
@@ -1,20 +1,18 @@
 /* C++ implementation of QuickSort Algorithm */
 #include <bits/stdc++.h> 
 using namespace std; 
-
-void swap(int* a, int* b) 
+void swap(int*a, int*b) 
 { 
 	int t = *a; 
 	*a = *b; 
 	*b = t; 
 } 
-
 int partition (int arr[], int low, int high) 
 { 
 	int pivot = arr[high];
-	int i = (low - 1); 
+	int i = (low-1); 
 
-	for (int j = low; j <= high - 1; j++) 
+	for(j = low; j <= high - 1; j++) 
 	{ 
 		if (arr[j] < pivot) 
 		{ 
@@ -25,7 +23,6 @@ int partition (int arr[], int low, int high)
 	swap(&arr[i + 1], &arr[high]); 
 	return (i + 1); 
 } 
-
 void quickSort(int arr[], int low, int high) 
 { 
 	if (low < high) 
@@ -35,21 +32,19 @@ void quickSort(int arr[], int low, int high)
 		quickSort(arr, pi + 1, high); 
 	} 
 } 
-
 void printArray(int arr[], int size) 
 { 
 	int i; 
 	for (i = 0; i < size; i++) 
-		cout << arr[i] << " "; 
-	cout << endl; 
+		count << arr[i] << " "; 
+	count << endl; 
 } 
-
 int main() 
 { 
 	int arr[] = {56, 43, 10, 91, 2, 45}; 
 	int n = sizeof(arr) / sizeof(arr[0]); 
 	quickSort(arr, 0, n - 1); 
-	cout << "Sorted array: \n"; 
+	count << "Sorted array: \n"; 
 	printArray(arr, n); 
 	return 0; 
 } 
