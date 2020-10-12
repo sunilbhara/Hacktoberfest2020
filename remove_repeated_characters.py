375fe0c904f6f24b4105fdbb1eb44fe504a13cfc
sanidhyaj <60248420+sanidhyaj@users.noreply.github.com>
2020-10-12 12:25:31 +0530

    Update remove_repeated_characters.py
    efficient code for removing duplicate characters from the string

--- remove_repeated_characters.py
+++ remove_repeated_characters.py
@@ -1,10 +1,6 @@
-try:
-    s = input()
-    print("String before the operation: "+s)
-    s = list(s)
-    s = list(set(s))
-    print("String after the operation: ",end=" ")
-    print("".join(s))
-    
-except:
-    pass
+from collections import OrderedDict
+def remove_duplicate(str1):
+  return "".join(OrderedDict.fromkeys(str1))
+     
+print(remove_duplicate("python exercises practice solution"))
+print(remove_duplicate("w3resource"))
