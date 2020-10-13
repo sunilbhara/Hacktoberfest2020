c997d7fd2d042158e3a44cec1dc112ee50da8a9d
Sreehari Premkumar <66044509+harimancodes@users.noreply.github.com>
2020-10-13 12:07:16 +0530

    Fibonacci using recursion!
    Reduced number of lines of code!

--- Java/Fibonacci.java
+++ Java/Fibonacci.java
@@ -1,14 +1,8 @@
 class fibonacci {
     public static long fib(int x) {
-        if(x<2)
-        return x;
-        int a=0,b=1,c=0;
-        for(int i=2;i<=x;i++){
-            c=a+b;
-            a=b;
-            b=c;
-        }
-        return c;
+          if (x <= 1) 
+            return x; 
+        return fib(x - 1) + fib(x - 2); 
     }
 
     public static void main(String args[]) {
