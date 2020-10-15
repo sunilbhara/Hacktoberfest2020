8dac26c10221f3a40df6107317e68c292562ac00
mahendra9661 <mahendrapurbia19@gmail.com>
2020-10-15 13:42:39 +0530

    Add files via upload
    variable Program (typecasting Excersie)

---
+++ Python/variableprogram(typecasting).py
@@ -0,0 +1,17 @@
+#variable program
+var1 = "50"
+var2 = "50"
+var3 = 50
+var4 = 90.5
+print(type (var1))
+print(type (var2))
+print(type (var3))
+print(type (var4))
+print(10* str(int(var1)) + str(int(var2)))
+print(10* "hello world\n")
+
+"""
+str()
+int()
+float()
+"""
