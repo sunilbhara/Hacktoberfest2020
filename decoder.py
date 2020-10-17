5a9a2db390674fea9ea0a4bf5c417c8e4da034a1
naaz <ak3502186@gmail.com>
2020-10-17 15:30:19 +0530

    added apython file for decoding a link containg text document

---
+++ decoder.py
@@ -0,0 +1,13 @@
+import socket
+
+mysock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org',80))
+cd='GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cd)
+
+while True:
+    dat=mysock.recv(512)
+    if len(dat)<1:
+        break
+    print(dat.decode())
+mysock.close()
