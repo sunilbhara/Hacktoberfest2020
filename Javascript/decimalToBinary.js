7457ed2c034a80952d977106056ffc26bb9d95f2
Sultan <73010596+sultanrif@users.noreply.github.com>
2020-10-20 21:04:23 +0300

    Update decimalToBinary.js
    Decimal to binary

--- Javascript/decimalToBinary.js
+++ Javascript/decimalToBinary.js
@@ -1,9 +1,13 @@
-function decimalToBinary(n) {
+// function for the decimal to binary
 
+function decimalToBinary(n) {
+// Declaration of array for storing binary number
     let binaryDigits = [];
+// Looping through the values
     while (n > 0) {
         binaryDigits.unshift(n % 2);
         n = Math.floor(n / 2);
     }
+// Array joining as binary number
     return binaryDigits.join("");
 }
