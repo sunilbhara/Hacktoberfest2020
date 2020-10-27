41305f24c73c043b7dbf628a14e47e1ee6b08b56
cawrites <54048339+cawrites@users.noreply.github.com>
2020-10-26 18:32:30 -0700

    Create basic-calculator.py
    Create a basic calculator using Python.

---
+++ Python/basic-calculator.py
@@ -0,0 +1,48 @@
+# Create a calculator in python to execute basic math functions. Good for K -8 School Aged Children and Up.
+# Download Python https://www.python.org/downloads/
+
+# Function adds two numbers
+def add(x, y):
+    return x + y
+
+# Function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# Function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# Function divides two numbers
+def divide(x, y):
+    return x / y
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # Take input from the user
+    choice = input("Select an operation(1/2/3/4): ")
+
+    # Check if operation selected is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        break
+    else:
+        print("Invalid Input")
