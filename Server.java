e1f5e673183a97235b39af6b97d2a5268ba6993e
Hasanga Lakdinu <36729333+HasangaLakdinu@users.noreply.github.com>
2020-10-27 15:06:35 +0530

    added second file

---
+++ Server.java
@@ -0,0 +1,14 @@
+
+package rmi;
+
+import java.rmi.Naming;
+
+
+public class Server {
+ public static void main(String[] args) throws Exception  
+ {
+     AddC obj = new AddC();
+     Naming.rebind("ADD",obj);
+     System.out.println("Server Started");
+ }
+}
