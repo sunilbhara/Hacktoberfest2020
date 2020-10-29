05ca337dd4e5ed76388146136ac4513548a6f669
fajarmuzaky <fajarmuzaky.project@gmail.com>
2020-10-29 10:02:05 +0700

    create algorithm pyramid pattern and pyramid continous character pattern with php

---
+++ PHP/pyramid_pattern.php
@@ -0,0 +1,11 @@
+<?php
+for( $a=10; $a>0; $a--){
+    for($i=1; $i<=$a; $i++){
+        echo "  ";
+    }for($a1=10; $a1>$a; $a1--){
+        echo"x";
+    }for($a2=11; $a2>$a; $a2--){
+        echo"x";
+    }echo"<br>";
+}
+?>
