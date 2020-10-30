e53de3421dc348d76f8c1298d8a138c459811348
Divya Garg <42246254+divyagar@users.noreply.github.com>
2020-10-30 13:06:19 +0530

    Create EuclideanAlgo.java

---
+++ Java/EuclideanAlgo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class EuclideanAlgo {
+	public static void main(String args[]) {
+		int a = 35, b = 10;
+		System.out.println(gcd(a, b));
+
+	}
+
+	private static int gcd(int a, int b) {
+		if(b <= 0)
+			return a;
+
+		return gcd(b, a%b);
+	}
+
+}
