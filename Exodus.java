199061bc76e1e7b83d4e5b716a2ae9eaef51e9b6
shubham31178 <73695974+shubham31178@users.noreply.github.com>
2020-10-31 21:28:22 +0530

    Exodus.java

---
+++ Exodus.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Sum
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+int s=0,a,b;
+System.out.println("Enter two numbers:");
+a=in.nextInt();
+b=in.nextInt();
+s=a+b;
+System.out.println("Sum:",s);
+}
+}
