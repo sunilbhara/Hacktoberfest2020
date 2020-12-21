124ab2d47c4bd7a17d7702e792abdfcf13dc5275
tanzil786 <tanzilidrisi68@gmail.com>
2020-12-21 13:04:28 +0530

    Update BubbleSort.java

--- Java/BubbleSort.java
+++ Java/BubbleSort.java
@@ -1,52 +1,53 @@
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 public class BubbleSort {
-
+ 
 	public static void main(String[] args)
-	{
-		int arr[] ={860,8,200,9};
-
-		System.out.println("---Array BEFORE Bubble Sort---");
-
-		printArray(arr);
-
-		bubbleSort(arr);//sorting array elements using bubble sort
-
-		System.out.println("---Array AFTER Bubble Sort---");
-
-		printArray(arr);
-
-	}
-	static void bubbleSort(int[] array)
-	{
-		int n = array.length;
-		int temp = 0;
+	{  
+		int arr[] ={860,8,200,9};  
+ 
+		System.out.println("---Array BEFORE Bubble Sort---"); 
+		
+	    printArray(arr);
+ 
+		bubbleSort(arr);//sorting array elements using bubble sort  
+ 
+		System.out.println("---Array AFTER Bubble Sort---");  
+		
+		printArray(arr); 
+ 
+	}  
+ 	static void bubbleSort(int[] array)
+	{  
+		int n = array.length;  
+		int temp = 0;  
 		for(int i=0; i < n; i++) // Iterating through the array length
-		{  System.out.println("Sort Pass Number "+(i+1));
+		{  System.out.println("Sort Pass Number "+(i+1)); 
 			for(int j=1; j < (n-i); j++)
-			{
-				System.out.println("Comparing "+ array[j-1]+ " and " + array[j]);
+			{  
+			    System.out.println("Comparing "+ array[j-1]+ " and " + array[j]);    
 				if(array[j-1] > array[j])
-				{
-
-					//swap elements
-					temp = array[j-1];
-					array[j-1] = array[j];
-					array[j] = temp;
-					System.out.println(array[j]  + " is greater than " + array[j-1]);
-					System.out.println("Swapping Elements: New Array After Swap");
+				{   
+				    
+					//swap elements  
+					temp = array[j-1];  
+					array[j-1] = array[j];  
+					array[j] = temp;  
+				    System.out.println(array[j]  + " is greater than " + array[j-1]);
+				    System.out.println("Swapping Elements: New Array After Swap");
 					printArray(array);
-				}
-
-			}
-		}
-
-	}
-
+				}  
+ 
+			}  
+		}  
+ 
+	} 
+	
 	static void printArray(int[] array){
-		System.out.println(Arrays.stream(array)
-				.mapToObj(String::valueOf)
-				.collect(Collectors.joining(",")));
+	    
+	    for(int i=0; i < array.length; i++)
+		{  
+			System.out.print(array[i] + " ");  
+		} 
+	    System.out.println();
+	    
 	}
 }
